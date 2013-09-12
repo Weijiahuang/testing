a72cd23976f861b8a8e3bb6c718655b15e4b30d1
Weijia Huang <hhuangweijia@gmail.com>
2013-09-11 20:29:16 -0400

    my first project

---
+++ P1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body bgcolor ="red">
+<center>
+<title> Welcome to letshangout </title>
+	<h1>
+		This is the webpage about me
+	<h1>
+<p>My first paragraph.</p>
+
+</center>
+</body>
+</html>
+
